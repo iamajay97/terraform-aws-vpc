4902e3450119b37b86faa90efbdc7a6069336288
Ajay M Patil <54729902+iamajay97@users.noreply.github.com>
2026-06-21 22:07:13 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,7 @@ output "internet_gateway_id" {
   description = "ID of the Internet Gateway"
   value       = aws_internet_gateway.igw.id
 }
+output "security_group_id" {
+  description = "ID of the Security Group"
+  value       = aws_security_group.web_sg.id
+}
